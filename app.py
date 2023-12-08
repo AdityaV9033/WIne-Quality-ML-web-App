0581268109805e0fba0117a190654404b458c155
AdityaV9033 <152848911+AdityaV9033@users.noreply.github.com>
2023-12-08 22:08:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,6 @@ x = df.iloc[:,:-1]
 y = df.iloc[:,-1]   
 print(x.shape)
 print(y.shape)
-print(type(x))
-print(type(y))
 
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=22)
